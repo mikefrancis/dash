af0136f2c1e58917a907ebff8fd60d0a853c857d
Mike Francis <mikeffrancis@gmail.com>
2016-12-02 09:28:56 +0000

    ES6

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-var http = require('https');
-var DashButton = require('dash-button');
+import http from 'https';
+import DashButton from 'dash-button';
 
-var button = new DashButton(process.env.DASH_BUTTON_MAC_ADDRESS);
+const SLACK_HOSTNAME = 'https://hooks.slack.com';
 
-var subscription = button.addListener(function () {
-	var request = http.request({
-        hostname: 'https://hooks.slack.com',
-        path: process.env.SLACK_HOOK_URL,
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        }
+const button = new DashButton(process.env.DASH_BUTTON_MAC_ADDRESS);
+
+button.addListener(() => {
+	const request = http.request({
+	    hostname: SLACK_HOSTNAME,
+	    path: process.env.SLACK_HOOK_URL,
+	    method: 'POST',
+		headers: { 'Content-Type': 'application/json' }
     });
 
     request.write({
         channel: process.env.SLACK_CHANNEL,
-        username: process.env.SLACK_USERNAME,
+        username: process.env.SLACK_USERNAME || 'Dash Button',
         text: process.env.SLACK_TEXT,
-        icon_emoji: process.env.SLACK_EMOJI
+        icon_emoji: process.env.SLACK_EMOJI || ':radio_button:'
     });
 
     request.end();
