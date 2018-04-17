7a98761e283813d6ed00f2f867dfde8a7e012af7
Mike Francis <mikeffrancis@gmail.com>
2018-04-17 22:14:31 +0100

    Added logging

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const DashButton = require('dash-button');
 
 const button = new DashButton(process.env.DASH_BUTTON_MAC_ADDRESS);
 
+console.log('----------');
+console.log('STARTED');
+console.log('----------');
+
 button.addListener(() => {
 	console.log('PRESSED MEEEEE');
 });
