fbb6e797a84b6f12c413bc66f8c84b0a51f0d3e3
Mike Francis <mikeffrancis@gmail.com>
2018-04-17 22:03:18 +0100

    Test

--- app.js
+++ app.js
@@ -1,24 +1,7 @@
-import http from 'https';
 import DashButton from 'dash-button';
-
-const SLACK_HOSTNAME = 'https://hooks.slack.com';
 
 const button = new DashButton(process.env.DASH_BUTTON_MAC_ADDRESS);
 
 button.addListener(() => {
-	const request = http.request({
-	    hostname: SLACK_HOSTNAME,
-	    path: process.env.SLACK_HOOK_URL,
-	    method: 'POST',
-		headers: { 'Content-Type': 'application/json' }
-    });
-
-    request.write({
-        channel: process.env.SLACK_CHANNEL,
-        username: process.env.SLACK_USERNAME || 'Dash Button',
-        text: process.env.SLACK_TEXT,
-        icon_emoji: process.env.SLACK_EMOJI || ':radio_button:'
-    });
-
-    request.end();
+	console.log('PRESSED MEEEEE');
 });
