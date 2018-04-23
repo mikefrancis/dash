f05495e02eaed27c08822346cd8214107d92d755
Mike Francis <mikeffrancis@gmail.com>
2018-04-23 19:06:45 +0100

    More tests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const axios = require('axios');
 const DashButton = require('dash-button');
 
 const button = new DashButton(process.env.DASH_BUTTON_MAC_ADDRESS);
@@ -8,6 +9,19 @@ console.log('----------');
 console.log('STARTED');
 console.log('----------');
 
-button.addListener(() => {
-	console.log('PRESSED MEEEEE');
+button.addListener(async () => {
+	try {
+        const response = await axios.post(process.env.NEXMO_API_URL, {
+            api_key: process.env.NEXMO_API_KEY,
+            api_secret: process.env.NEXMO_API_SECRET,
+            from: 'The RockPool',
+            text: "There's someone at the door",
+            to: process.env.PHONE_NUMBER,
+        });
+
+        console.log('MESSAGE SENT');
+        console.info(response.data);
+	} catch (error) {
+		console.error(error);
+	}
 });
